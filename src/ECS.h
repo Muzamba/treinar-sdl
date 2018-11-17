cc6d34da668bb2dc4114e69a39a09a87792512eb
Ariel Batista <batistariel98@gmail.com>
2018-11-17 04:05:40 -0200

    ajeitando ECS.h

--- src/ECS.h
+++ src/ECS.h
@@ -12,14 +12,15 @@ class Entity;
 
 using ComponentID = std::size_t;
 
-inline ComponentID getComponentTypeID(){
+
+inline ComponentID getComponentTypeID() {
     static ComponentID lastID = 0;
     return lastID++;
 }
 
-template <typename T> inline ComponentID getComponentTypeID() noexcept {
+template<typename T>inline ComponentID getComponentTypeID() noexcept{
     static ComponentID typeID = getComponentTypeID();
-    return typeID;
+    return typeID();
 }
 
 constexpr std::size_t maxComponents = 32;
@@ -27,7 +28,7 @@ constexpr std::size_t maxComponents = 32;
 using ComponentBitSet = std::bitset<maxComponents>;
 using ComponentArray = std::array<Component*, maxComponents>;
 
-class Component{
+class Component {
 public:
     Entity* entity;
 
@@ -38,6 +39,7 @@ public:
     virtual ~Component() {}
 };
 
+
 class Entity {
 private:
     bool active = true;
@@ -45,62 +47,28 @@ private:
 
     ComponentArray componentArray;
     ComponentBitSet componentBitSet;
-
 public:
-    void update()
-    {
+    void update() {
         for(auto& c : components) c->update();
         for(auto& c : components) c->draw();
-
     }
-    void draw();
-    bool isActive() const { return active; }
+    void draw() {}
+    bool isActive(){ return active; }
     void destroy() { active = false; }
 
-    template <typename T> bool hasComponent() const {
+    template<typename T> bool hasComponent() const {
         return componentBitSet[getComponentID<T>];
     }
 
-    template <typename T, typename... TArgs>
-    T& addComponent(TArgs&&...mArgs){
+    template<typename T, typename...TArgs>
+    T& addComponent(TArgs&&... mArgs) {
         T* c(new T(std::forward<TArgs>(mArgs)...));
-        c->Entity = this;
+        c->entity = this;
         std::unique_ptr<Component> uPtr{ c };
-        conmponets.emplace_back(std::move(uPtr));
+        components.emplace_back(std::move(uPtr));
 
         componentArray[getComponentTypeID<T>()] = c;
-        componentBitSet[getComponentTypeID<T>()] = true;
-
-        c->init();
-        return *c;
-    }
-
-    template<typedef T> T& getComponent() cont {
-        auto ptr(ComponentArray[getComponentTypeID<T>()]);
-        return *static_cast<T*>(ptr);
+        
     }
 };
 
-class Manager {
-private:
-    std::vector<std::unique_ptr<Entity>> entities;
-public:
-    void update(){
-        for(auto& e : entities) e->update;
-    }
-    void draw(){
-        for (auto& e : entities) e->draw;
-    }
-    void refresh(){
-        entities.erase(std::remove_if(std::begin(entities), std::end(entities),
-            [](const std::unique_ptr<Entity> &mEntity){
-                return !mEntity->isActive();
-        }),std::end(entities));
-    }
-    Entity& addEntity(){
-        Entity* e = new Entity();
-        std::unique_ptr<Entity> uPtr{e};
-        entities.emplace_back(std::move(uPtr));
-        return *e;
-    }
-};
